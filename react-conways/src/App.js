e28a5c8ab0d7d3861c8a1300037866f31770fb40
lilvina <davinaleo2007@yahoo.com>
2019-07-16 17:11:18 -0700

    got board to appear on the page

--- react-conways/src/App.js
+++ react-conways/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import Game from './components/Game.js';
 function App() {
   return (
     <div className="App">
       <h2>Conway's Life</h2>
+      <Game />
     </div>
   );
 }
